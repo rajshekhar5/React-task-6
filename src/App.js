e9853edf41c4fcc535963de94fb9bad7fcbcf1f8
Raj Shekhar <rajshekhar2may2002@gmail.com>
2023-11-03 00:40:04 +0530

    s commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="header bg-primary text-white py-5 text-center">
-        <h1 className="name">Yuvraj Saha</h1>
+        <h1 className="name">Raj Shekhar</h1>
         <p className="job-title">Full Stack Developer</p>
       </header>
 
@@ -22,7 +22,7 @@ function App() {
       <section className="contact py-5 bg-primary text-white">
         <div className="container">
           <h2 className="section-title text-white">Contact Me</h2>
-          <p>Email: yuvrajsaha51103@gmail.com</p>
+          <p>Email: rajshekhar2may2002@gmail.com</p>
           <p>LinkedIn: <a href="https://www.linkedin.com/in/yuvraj-saha-8a5ba61b4/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
           <p>GitHub: <a href="https://github.com/yuvi511" target="_blank" rel="noopener noreferrer">GitHub Profile</a></p>
         </div>
